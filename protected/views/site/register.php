7894f521d2ddd0120844dfd0c5c0a6cc04d0bef9
Tomasz Biniecki <thomas.biniecki@nccgroup.com>
2013-09-16 17:32:36 +0100

    registration view

---
+++ protected/views/site/register.php
@@ -0,0 +1,27 @@
+<div class="span5">
+    <h3>Register</h3>
+    <?php $form=$this->beginWidget('CActiveForm'); ?>
+
+    <?php echo $form->errorSummary($model); ?>
+
+    <div class="row">
+        <?php echo $form->label($model,'username'); ?>
+        <?php echo $form->textField($model,'username') ?>
+    </div>
+
+    <div class="row">
+        <?php echo $form->label($model,'password'); ?>
+        <?php echo $form->passwordField($model,'password') ?>
+    </div>
+
+    <div class="row">
+        <?php echo $form->label($model,'password repeat'); ?>
+        <?php echo $form->passwordField($model,'password_repeat') ?>
+    </div>
+
+    <div class="row submit">
+        <?php echo CHtml::submitButton('Register'); ?>
+    </div>
+
+    <?php $this->endWidget(); ?>
+</div><!-- form -->
