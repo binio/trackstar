1296a9e2de755aa3f672c31fe188309285a77b0d
Tomasz Biniecki <thomas.biniecki@nccgroup.com>
2013-09-16 17:33:02 +0100

    registration model

--- protected/models/RegisterForm.php
+++ protected/models/RegisterForm.php
@@ -1,1 +1,22 @@
 <?php
+
+class RegisterForm extends CFormModel
+{
+    public $username;
+    public $password;
+    public $password_repeat;
+
+    public function register()
+    {
+
+    }
+
+    public function rules()
+    {
+        return array(
+            array('username, password', 'required'),
+            array('password_repeat', 'required', 'on'=>'register'),
+            array('password', 'compare', 'compareAttribute'=>'password_repeat', 'on'=>'register'),
+        );
+    }
+}
