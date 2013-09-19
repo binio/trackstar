5c0fb7ca0cc7e470d0718bfa0e3df767cf63af0b
Tomasz Biniecki <thomas.biniecki@nccgroup.com>
2013-09-20 00:18:12 +0100

    unique validator

--- protected/models/RegisterForm.php
+++ protected/models/RegisterForm.php
@@ -14,9 +14,10 @@ class RegisterForm extends CFormModel
     public function rules()
     {
         return array(
-            array('username, password', 'required'),
+            array('username, password', 'required'),//add unique validator and try to insert to db
             array('password_repeat', 'required'),
             array('password', 'compare', 'compareAttribute'=>'password_repeat'),
+            array('username','unique', 'className' => 'User'),
         );
     }
 }
