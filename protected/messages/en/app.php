cc7c957484359d2e6291e84da55d72c712b1f126
binio <tbiniecki@gmail.com>
2013-09-30 11:54:47 +0100

    translation files

---
+++ protected/messages/en/app.php
@@ -0,0 +1,8 @@
+<?php
+//Yii::t('model.RegisterForm.username','TWOJ LOGIN')
+return array(
+    'model.register.username' =>'User name',
+    'model.register.email' =>'Email',
+    'model.register.password' =>'Password',
+    'model.register.password_repeat' =>'Password repeat',
+);
---
+++ protected/messages/pl/app.php
@@ -0,0 +1,8 @@
+<?php
+//Yii::t('model.RegisterForm.username','TWOJ LOGIN')
+return array(
+    'model.register.username' =>'Twoj Login',
+    'model.register.email' =>'Adres Email',
+    'model.register.password' =>'Hasło',
+    'model.register.password_repeat' =>'Powtorz Hasło',
+);
