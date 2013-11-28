dfa75e2aa82e988a0bee40ce738082efbc748eec
binio <tbiniecki@gmail.com>
2013-11-28 10:40:31 +0000

    scafold for mobile app

--- protected/controllers/ProjectController.php
+++ protected/controllers/ProjectController.php
@@ -31,10 +31,10 @@ class ProjectController extends Controller
                 'actions'=>array('index','view', 'adduser', 'create', 'update'),
                 'users'=>array('@'),
             ),
-//            array('allow', // allow authenticated user to perform 'create' and 'update' actions
-//                'actions'=>array('create','update'),
-//                'users'=>array('@'),
-//            ),
+            array('allow', // allow authenticated user to perform 'create' and 'update' actions
+                'actions'=>array('ts'),
+                'users'=>array('*'),
+            ),
             array('allow', // allow admin user to perform 'admin' and 'delete' actions
                 'actions'=>array('admin','delete'),
                 'users'=>array('admin'),
@@ -194,6 +194,13 @@ class ProjectController extends Controller
 			Yii::app()->end();
 		}
 	}
+
+    public function actionTs(){
+
+        $this->layout = 'ts';
+        $this->render('ts',array());
+    }
+
 
     public function actionAdduser($pid)
     {
