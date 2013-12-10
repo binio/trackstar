aa56a638c23b21e326fa5861b19e14eb4da82595
binio <tbiniecki@gmail.com>
2013-12-10 17:37:17 +0000

    add copy to index and about page

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -6,7 +6,26 @@ $this->breadcrumbs=array(
 	'About',
 );
 ?>
-<h1>About</h1>
+<h1>O Serwisie</h1>
 
-<p>This is a "static" page. You may change the content of this page
-by updating the file <code><?php echo __FILE__; ?></code>.</p>
+<div class="row">
+    <div class="span4">
+        Modlitwa często przychodzi z trudem ale w momencie gdy pojawia się drugi człowiek zaczyna być łatwiej.
+        Widząc, że w naszym dążeniu do świętości nie jesteśmy sami jest nam łatwiej, modlimy się wtedy częściej i dłużej.
+        Tam gdzie dwóch lub więcej modli się tam Bóg jest między nimi.
+    </div>
+    <div class="span4">
+        Serwis ten ma być odpowiedzią na chęć dołączenia do grupy modlitewnej i uczestniczenia we wsþolnych modlitwach.
+        Modląc sie wsþólnie za pośrednictwem internetu mamy globalny zasięg wiec możemu tworzyć nieograniczone koła modlitewne.
+        Pamietajmy jednak aby jednak zawsze w centrum znalazł się Bóg. Zanośmy nasze modlitwy przed Jego oblicze.
+    </div>
+    <div class="span4">
+        Serwis pozwoli na tworzenie własnych intencji, modlenie sie w intencjach swoich i innych urzytkowników serwisu.
+        Każdy bedzie mieć podgląd w intencje oraz ilość osób modlących sie w intencjach. Urzytkownicy będą mogli dołaczać do
+        do grup modlitewnych jak i tworzyć nowe grupy. W pózniejszym okresie zostanie tez wprowarzony terminarz tak abyśmy mogli dodawać modlitwy do niego i
+        wspólnie się modlić w tym samym czasie.
+    </div>
+</div>
+
+
+
