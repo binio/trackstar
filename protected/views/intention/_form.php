1a9849448cb1bb9dd1e31b23eee5b7a2637902e9
binio <tbiniecki@gmail.com>
2013-12-17 01:01:52 +0000

    form to utf-8

--- protected/views/intention/_form.php
+++ protected/views/intention/_form.php
@@ -9,6 +9,7 @@
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'intention-form',
 	'enableAjaxValidation'=>false,
+    'htmlOptions'=>array('accept-charset'=>'UTF-8'),
 )); ?>
 
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
